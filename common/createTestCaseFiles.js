db3651962f7f5051ba8b72868081b36aed27bd53
Epic Berry <epicbrry@gmail.com>
2016-11-22 21:09:10 -0500

    protractor create files and run using conf.js

---
+++ common/createTestCaseFiles.js
@@ -0,0 +1,97 @@
+var http = require('http');
+
+PrepareTests();
+
+function PrepareTests(){
+  var options = {
+    host: '192.169.179.82',
+    port: 1111,
+    path: '/data/all'
+  };
+
+  http.get(options, function(res){
+    res.on('data', function(chunk){
+      //console.log(JSON.parse(chunk));
+      CreateFiles(JSON.parse(chunk));
+    });
+  }).on("error", function(e){
+    console.log("Got error: " + e.message);
+  });
+}
+
+
+function CreateFiles(testCases){
+  testCases.forEach(function(testCase) {
+     //var editedTestCase = "describe('Amazon shopping', function() { it('should open an item', function() { browser.get('https://www.amazon.com'); element(by.css('[id=\"twotabsearchtextbox\"]')).sendKeys('"+item.testSteps[1].value+"'); element(by.xpath('[//*[@id=\"result_1\"]/div/div/div/div[2]/div[2]/a]')).click(); }); });"
+    var strTestFlow = CreateProtractorString(testCase);
+
+    var fs = require('fs');
+    fs.writeFile("../testFiles/"+testCase.testCaseId+".js", strTestFlow, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        //console.log("The file was saved!");
+    });
+  });
+}
+
+function CreateProtractorString(testCase){
+  //return "describe('Amazon shopping', function() { it('should open an item', function() { browser.get('https://www.amazon.com'); element(by.css('[id=\"twotabsearchtextbox\"]')).sendKeys('"+testCase.testSteps[1].value+"'); element(by.css('[value=\"Go\"]')).click();   }); });"
+  var strTestFlow = "describe('" + testCase.description + "' , function() { it('" + testCase.description + "', function() {";
+  testCase.testSteps.forEach(function(testStep) {
+    switch(testStep.type) {
+        case 'navigate':
+            strTestFlow += "\n browser.get('" + testStep.url + "');"
+            break;
+        case 'input':
+            strTestFlow += "\n element(by.css('[" + testStep.elementName + "]')).sendKeys('" + testStep.value + "');";
+            break;
+        case 'click':
+            strTestFlow += "\n element(by.css('[" + testStep.elementName + "]')).click();";
+            break;
+        default:
+            console.log('In default');
+      }
+  });
+  strTestFlow += "\n }); \n });";
+  return strTestFlow;
+}
+
+// function CreateProtractorString(testCase){
+//   //return "describe('Amazon shopping', function() { it('should open an item', function() { browser.get('https://www.amazon.com'); element(by.css('[id=\"twotabsearchtextbox\"]')).sendKeys('"+testCase.testSteps[1].value+"'); element(by.css('[value=\"Go\"]')).click();   }); });"
+//   var strTestFlow = "describe('" + testCase.description + "' , function() { it('" + testCase.description + "', function() {";
+//   var strEndFlow = "";
+//   testCase.testSteps.forEach(function(testStep) {
+//     switch(testStep.type) {
+//         case 'navigate':
+//             strTestFlow += "\n browser.get('" + testStep.url + "').then(function(){"
+//             strEndFlow += "\n}, function(err){\n throw new Error('Error occurred'); \n});";
+//             break;
+//         case 'input':
+//             strTestFlow += "\n element(by.css('[" + testStep.elementName + "]')).sendKeys('" + testStep.value + "').then(function(){";
+//             strEndFlow += "\n}, function(err){\n throw new Error('Error occurred'); \n});";
+//             break;
+//         case 'click':
+//             strTestFlow += "\n element(by.css('[" + testStep.elementName + "]')).click().then(function(){";
+//             strEndFlow += "\n}, function(err){\n throw new Error('Error occurred'); \n});";
+//             break;
+//         default:
+//             console.log('In default');
+//       }
+//   });
+//   strTestFlow += strEndFlow + "\n }); \n });";
+//
+//   return strTestFlow;
+// }
+
+// //if body is sent in chunks
+// request.on('response', function (response) {
+//   var body = '';
+//   response.on('data', function (chunk) {
+//     body += chunk;
+//   });
+//   response.on('end', function () {
+//     console.log('BODY: ' + body);
+//   });
+// });
+// request.end();
---
+++ testFiles/conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+  seleniumAddress: 'https://param7:f0cfe8f5-812c-4ba3-8045-9d3785b25295@ondemand.saucelabs.com:443/wd/hub',
+  specs: ['*.js'],
+  //specs: ['1001.js'],
+  multiCapabilities: [{
+      'browserName': 'chrome',
+      shardTestFiles: true,
+      maxInstances: 10
+    }
+    // , {
+    //   'browserName': 'chrome'
+    // }
+  ],
+    onPrepare: function(){
+       browser.ignoreSynchronization = true;
+    }
+};
